c339bbab66b69fc990ceffa857610019e34f59ff
Torres1982 <B00073668@student.itb.ie>
2017-04-04 00:59:15 +0100

    file renamed

--- app/src/main/java/com/torres/companionshipapp/Map.java
+++ app/src/main/java/com/torres/companionshipapp/Map.java
@@ -3,10 +3,10 @@ package com.torres.companionshipapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class HobbyDelete extends AppCompatActivity {
+public class Map extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.hobby_delete);
+        setContentView(R.layout.map_places);
     }
 }
