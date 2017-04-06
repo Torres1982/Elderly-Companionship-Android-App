cf2b13781c0bf83f91acd45e53de8a3bfbfdefe0
Torres1982 <B00073668@student.itb.ie>
2017-04-07 00:41:49 +0100

    File updated

--- app/src/main/java/com/torres/companionshipapp/FriendsFinder.java
+++ app/src/main/java/com/torres/companionshipapp/FriendsFinder.java
@@ -12,7 +12,6 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
