7688dc0dbe0c0d6eddcd0f2400002e2492460b60
Torres1982 <B00073668@student.itb.ie>
2017-04-07 12:16:47 +0100

    File updated

--- app/src/main/java/com/torres/companionshipapp/FriendsList.java
+++ app/src/main/java/com/torres/companionshipapp/FriendsList.java
@@ -1,4 +1,13 @@
 package com.torres.companionshipapp;
 
-public class FriendsList {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FriendsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.friends_list);
+    }
 }
