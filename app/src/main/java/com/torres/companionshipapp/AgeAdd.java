b8c0ecf69a692a2a9e9920be034853ce71e8ccdd
Torres1982 <B00073668@student.itb.ie>
2017-04-08 18:25:31 +0100

    File created

--- app/src/main/java/com/torres/companionshipapp/AgeAdd.java
+++ app/src/main/java/com/torres/companionshipapp/AgeAdd.java
@@ -1,7 +1,154 @@
 package com.torres.companionshipapp;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AgeAdd extends AppCompatActivity {
 
+    // Declare global variables and objects
+    Button addMyAgeButton;
+    String message;
+    String spinnerValue;
+    Spinner spinner;
+    String userId;
+    FirebaseAuth firebaseAuthentication;
+    DatabaseReference databaseReference;
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.age_add);
+
+        // Get the Firebase authentication instance
+        firebaseAuthentication = FirebaseAuth.getInstance();
+
+        // Retrieve the reference of the objects from the hobby_add.xml file
+        addMyAgeButton = (Button)findViewById(R.id.button_age_add);
+        spinner = (Spinner) findViewById(R.id.age_add_spinner);
+
+        // Call the supportive methods
+        setUpActionBar();
+        setUpSpinner();
+
+        // Call the methods with assigned Listeners
+        addListenerToAgeAddButton();
+    }
+
+    // *********************************************************************************************
+    // ******************** Register Listener for the Add Hobby Button *****************************
+    // ******************** Add hobby to the Database **********************************************
+    // *********************************************************************************************
+    public void addListenerToAgeAddButton () {
+        addMyAgeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Get the value from the selected dropdown list
+                spinnerValue = String.valueOf(spinner.getSelectedItem());
+                showDialog();
+                addAgeToFirebaseDatabase();
+            }
+        });
+    }
+
+    // *********************************************************************************************
+    // ******************** Set up custom Action Bar title *****************************************
+    // ******************** Add a logo to the Action Bar *******************************************
+    // *********************************************************************************************
+    public void setUpActionBar() {
+
+        ActionBar myCustomActionBar = getSupportActionBar();
+
+        // Disable default Action Bar settings
+        myCustomActionBar.setDisplayShowHomeEnabled(false);
+        myCustomActionBar.setDisplayShowTitleEnabled(false);
+
+        LayoutInflater myLayoutInflater = LayoutInflater.from(this);
+        View myCustomView = myLayoutInflater.inflate(R.layout.custom_action_bar, null);
+
+        // Get the reference from the action_bar_title.xml file
+        TextView myTitleTextView = (TextView) myCustomView.findViewById(R.id.action_bar_title);
+
+        // Set up custom Action Bar
+        String actionBarTitle = "Elderly Companionship";
+        myTitleTextView.setText(actionBarTitle);
+        myCustomActionBar.setCustomView(myCustomView);
+        myCustomActionBar.setDisplayShowCustomEnabled(true);
+    }
+
+    // *********************************************************************************************
+    // ******************** Instantiate the Custom Dialog class ************************************
+    // ******************** Retrieve the Custom Dialog View ****************************************
+    // *********************************************************************************************
+    private void showDialog() {
+
+        // Get the message to display on dialog
+        constructSharedPreferences();
+
+        CustomDialog customDialog = new CustomDialog();
+        customDialog.setCancelable(false);
+        customDialog.show(getSupportFragmentManager(), "CUSTOM DIALOG");
+    }
+
+    // *********************************************************************************************
+    // ******************** Create Shared Preferences *************************************************
+    // ******************** Send the title for the Custom Dialog ***********************************
+    // *********************************************************************************************
+    public void constructSharedPreferences() {
+
+        String key = "message";
+        message = " Your age: " + spinnerValue + " has been added to the list";
+
+        // Create object of Shared Preferences
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        // Get Editor
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        // Assign the string value for the Custom Dialog title
+        editor.putString(key, message);
+        // Commit the Edit
+        editor.apply();
+    }
+
+    // *********************************************************************************************
+    // ******************** Set up custom Spinner **************************************************
+    // ******************** Edit Spinner text size and colors **************************************
+    // *********************************************************************************************
+    public void setUpSpinner() {
+
+        ArrayAdapter spinnerArrayAdapter = ArrayAdapter.createFromResource(this, R.array.age_array, R.layout.spinner_edit);
+        spinner.setAdapter(spinnerArrayAdapter);
+    }
+
+    // Add a user and details to Database
+    public void addAgeToFirebaseDatabase() {
+
+        // Get the Database reference
+        databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://companionship-app.firebaseio.com/");
+
+        // Get the Firebase User instance
+        firebaseUser = firebaseAuthentication.getCurrentUser();
+
+        // Retrieve Firebase User Id for currently logged in user
+        if (firebaseUser != null) {
+            userId = firebaseUser.getUid();
+        }
+
+        // Save User's details in the Firebase database (at root "users")
+        databaseReference.child("users").child(userId).child("age").setValue(spinnerValue);
+    }
 }
