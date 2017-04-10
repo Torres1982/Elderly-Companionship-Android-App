fc828109351af1e3128182f6ef80a3a278392430
Torres1982 <B00073668@student.itb.ie>
2017-04-11 00:21:21 +0100

    File updated

--- app/src/main/java/com/torres/companionshipapp/EventsFinderDate.java
+++ app/src/main/java/com/torres/companionshipapp/EventsFinderDate.java
@@ -6,34 +6,43 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class EventsFinderDate extends AppCompatActivity {
 
     TextView displayDateTextView;
     Button findEventsByDateButton;
+    DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.events_finder_date);
-        
+
         // Retrieve the reference of the objects from the profile_page.xml file
-        displayDateTextView = (TextView) findViewById(R.id.today_date);
+        displayDateTextView = (TextView) findViewById(R.id.text_view_date_chooser);
         findEventsByDateButton = (Button)findViewById(R.id.find_events_by_date);
+        datePicker = (DatePicker)findViewById(R.id.date_picker);
 
         // Call the methods with associated Listeners
         addListenerToFindEventsByDateButton();
     }
 
     // *********************************************************************************************
-    // ******************** Register Listener for the Find Friends Button **************************
-    // ******************** Redirect ... ***********************************************************
+    // ******************** Register Listener for the Find Events by Date Button *******************
     // *********************************************************************************************
     public void addListenerToFindEventsByDateButton () {
         findEventsByDateButton.setOnClickListener(new View.OnClickListener() {
             public void onClick (View view) {
+                // Retrieve the values for day, month and year from a Date Picker
+                String day = "Day = " + datePicker.getDayOfMonth();
+                // Month starts at 0 so we need to add 1
+                String month = "Month = " + (datePicker.getMonth() + 1);
+                String year = "Year = " + datePicker.getYear();
 
+                Toast.makeText(getApplicationContext(), day + "\n" + month + "\n" + year, Toast.LENGTH_LONG).show();
             }
         });
     }
