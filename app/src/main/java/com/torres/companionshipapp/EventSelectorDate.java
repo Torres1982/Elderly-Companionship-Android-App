c94da8bfa22d556ab112e485fb165fcb3d65aed4
Torres1982 <B00073668@student.itb.ie>
2017-04-11 15:03:20 +0100

    File created

---
+++ app/src/main/java/com/torres/companionshipapp/EventSelectorDate.java
@@ -0,0 +1,12 @@
+package com.torres.companionshipapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EventSelectorDate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
