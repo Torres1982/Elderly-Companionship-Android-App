389b24b247d72d093116c152b80b61e1c600b515
Torres1982 <B00073668@student.itb.ie>
2017-04-11 15:48:02 +0100

    New intent retrieved

--- app/src/main/java/com/torres/companionshipapp/EventSelectorDate.java
+++ app/src/main/java/com/torres/companionshipapp/EventSelectorDate.java
@@ -2,11 +2,33 @@ package com.torres.companionshipapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class EventSelectorDate extends AppCompatActivity {
+
+    String eventName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_selector_date);
+
+        retrieveEventNameFromIntent();
+    }
+
+    // *********************************************************************************************
+    // ******************** Get intent with the Event Name *****************************************
+    // *********************************************************************************************
+    public void retrieveEventNameFromIntent() {
+
+        String key = "spinnerValue";
+
+        // Retrieve the Event Name from EventSelectorName activity
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            eventName = extras.getString(key);
+            Toast.makeText(getApplicationContext(), eventName, Toast.LENGTH_LONG).show();
+        }
     }
 }
