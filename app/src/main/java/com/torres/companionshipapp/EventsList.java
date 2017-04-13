c23f96b0c44422ec6e0246a3f94ce4ea32a76fa6
Torres1982 <B00073668@student.itb.ie>
2017-04-13 02:22:46 +0100

    File created

---
+++ app/src/main/java/com/torres/companionshipapp/EventsList.java
@@ -0,0 +1,13 @@
+package com.torres.companionshipapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EventsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.events_list);
+    }
+}
