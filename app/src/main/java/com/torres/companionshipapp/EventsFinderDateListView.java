4bba94e55c38063149239e199f8f5dbedaa1d22f
Torres1982 <B00073668@student.itb.ie>
2017-04-14 16:53:19 +0100

    File created

---
+++ app/src/main/java/com/torres/companionshipapp/EventsFinderDateListView.java
@@ -0,0 +1,14 @@
+package com.torres.companionshipapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EventsFinderDateListView extends AppCompatActivity {
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.events_finder_date_list_view);
+    }
+}
